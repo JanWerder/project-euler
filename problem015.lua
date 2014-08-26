8b6200d7b205356df0f218fc9da154ad20fe6d9b
Jan <jackflash5500@gmail.com>
2014-08-26 10:44:54 +0200

    Problem 15

---
+++ problem015.lua
@@ -0,0 +1,17 @@
+function binomial( n, k )
+    if k > n then
+		return nil
+	end
+    if k > n/2 then
+		k = n - k
+	end       --   (n k) = (n n-k)
+
+    numer, denom = 1, 1
+    for i = 1, k do
+        numer = numer * ( n - i + 1 )
+        denom = denom * i
+    end
+    return numer / denom
+end
+
+print(binomial(40,20))
